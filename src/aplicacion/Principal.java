bcab4a03e502745923c1f339267a464f7f849eb0
Jaime <symptomjaime@gmail.com>
2023-10-09 11:39:20 +0200

    Codigo antes de compilar

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,7 @@
+package aplicacion;
+import presentacion.*;
+public class Principal{
+	public static void main(String[] args){ 
+		InterfazUsuario.ejecutar(args);
+	}
+}
---
+++ src/dominio/OficinaPadron.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+package dominio;
+
+public class OficinaPadron{
+	private ArrayList<Habitante> habitantesPadron = new ArrayList<>();
+
+	public void annadir(Habitante habitante){ 	
+		habitantesPadron.add(habitante);
+		volcarContactos();
+	}
+
+	public ArrayList getHabitantesPadron(){ 	
+		return habitantesPadron;
+	}
+	public int calcularNumeroHabitantes(){ 	
+		int suma = 0;
+		for(Habitante habitante : habitantesPadron){ 	
+			suma++;
+		}
+		return suma;
+	}
+	private void cargarHabitantes(){ 	
+		try{
+			File fichero = new File("padron.csv");
+			// Crea el fichero si no existe: 
+			fichero.createNewFile();
+			Scanner sc = new Scanner(fichero); 
+			sc.useDelimiter(",|\n"); 
+			while(sc.hasNext()){
+				Habitante habitante = new Habitante(sc.next(),sc.next(),sc.next());
+				habitantesPadron.add(habitante);
+			}
+			sc.close();
+		}catch(IOException ex){
+			System.out.println("No hay habitantes inscritos");
+		}
+	}
+	public OficinaPadron(){ 
+		cargarHabitantes();
+	}
+
+	public void volcarContactos(){ 
+		try{
+			FileWriter fw = new FileWriter("padron.csv"); 
+			for(Habitante habitante : habitantesPadron){
+				fw.write(habitante.getNombre() + "," + habitante.getApellido1() + "," + habitante.getApellido2() + "\n");
+			}
+			fw.close();
+		}catch(IOException ex){ 
+			System.err.println(ex);
+		}
+}
+
